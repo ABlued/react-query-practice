3cab10aeb0d4ed599a0b66cf97236620d3dba00a
ABlued <53801395+ABlued@users.noreply.github.com>
2022-01-10 21:54:02 +0900

    동적쿼리 실습

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import react, { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Navbar } from './components/Navbar';
+import { People } from './components/People';
+import { Planets } from './components/Planets';
+import { ReactQueryDevtools } from 'react-query/devtools';
+const queryClient = new QueryClient();
+
+function App() {
+	const [page, setPage] = useState('');
+	return (
+		<>
+			<QueryClientProvider client={queryClient}>
+				<div className="App">
+					<h1>Star Wars Info</h1>
+					<Navbar setPage={setPage} />
+					<div className="content">
+						{page === 'planets' ? <Planets /> : <People />}
+					</div>
+				</div>
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
+		</>
+	);
+}
+
+export default App;
